5c48cc7cf2298b3ebe77003cf574f2afd3ed5355
Haotian Li <68355398+Fauq@users.noreply.github.com>
2024-04-13 13:43:24 -0400

    New Version

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
 from math import atan2
-import math
-import random
-import pygame
-import time
+import math, random, pygame, time
 import numpy as np
 from pygame.locals import *
-import json 
-import csv
-import os
-import pickle
+import json, csv, os, pickle
 
+from calibrationASL import runCalibration_ASL
+
+# Setting game sessions and parameters
+calib_flag = 0
+gameplay_flag = 1
+
+
+
+#======================= Calibration Phase ====================================
+if calib_flag:
+    C_glove, p0, synergies, scaling, dxy, calibFlag = runCalibration_ASL(calibSigns=33, subject='001', joint_dim=21, expDay=1)
+else:
+    C_glove= np.array([[-0.03132698,  0.02051033,  0.03498236,  0.03498232,  0.08300048, -0.08430041,  0.40687442,  0.58010444,  0.52293686, -0.14560099, 0.88047125,  1.23860548,  1.11474462,  0.0058947,  0.62157013, 0.88795741,  0.79916169,  0.03217047,  0.52628314,  0.74993886, 0.6766496 ],
+                     [ 0.46436904, -0.30624316,  2.06743546,  2.06743242,  4.37282329, 0.70592261, -2.05631285, -2.90577053, -2.64021756, -1.69479561, 1.8350689 ,  2.74689286,  2.47220197, -0.40117436, -0.8480112, -1.2114465 , -1.09030442, -0.27762784, -0.83292699, -1.19548129, -1.07090373]])
+    p0 = np.array([[  34.06299109],
+                   [-180.81036949]])
+
+C_joy = 3 * np.array([[3,0,7,5],[0,1,7,2]])
+    
+#======================= Gameplay Phase =======================================
 pygame.init()
 screen = pygame.display.set_mode((1150, 670))
 pygame.display.set_caption("Capture The Flag")
 clock = pygame.time.Clock()
 running = True
 plugged = False
-captured = False
-color = (0, 0, 255)
-trial = 1
+attackerColor = (0, 0, 255)
 game_data = []
 start_time = time.time()
-path = 'DaqCpp/record/pose.csv'
-arr = []
-arr1 = []
-attackerCaptured = False 
+filePath_gloveData = 'DaqCpp/record/pose.csv'
 
+# flags for condition checking
+flagCaptured = False
+attackerCaptured = False
+flagReturned = False 
+returnHome_flag = False
+trialEnd_flag = False
+hasFlag = False
 
+# trials and sessions
+trial = 1
+session = 1 
 
-
-
-    #arr1 = next(reader)
-#joystick
-
-
+# Checking joystick connection and creating the joystick object
 pygame.joystick.init()
 if pygame.joystick.get_count() > 0:
     plugged = True
@@ -52,14 +66,6 @@ timer = time.time()
 
 randx = [67, 200]
 randy = [67, 333, 598]
-#m_arr = np.random.randint(0, 8, size=(2, 4))
-
-#print(m_arr)
-
-
-m_arr1 = np.array([[3,0,7,5],[0,1,7,2]])
-m_arr1 = 3*m_arr1
-m_arr = np.array([[-5.33247879150843e-05,3.55507427725192e-05,-0.000668516195280432, -0.000631942988468327,	-0.00143781984715076,	-0.000691431808599448,	0.00276886989301726,	0.00395505401419451,	0.00355954996564003,	-3.74330758317242e-05,	0.00697306630302672,	0.00893695172258586,	0.00804325691094529,	7.00739928822353e-05,	0.00569227277209805,	0.00746988789276899,	0.00672289803153796,	-1.85044318698863e-05,	0.00504560935962856,	0.00635550797765998,	0.00571995714367466],[0.00136859144602875,	-0.000912386033903697,	0.0166398580212531,	0.0170947439600576,	0.0340428744699597,	0.00160655988197229,	-0.00393386205170458,	-0.00561862284449718,	-0.00505676166756256,	0.00249110872571687,	0.00729916518055413,	0.0119279744390449,	0.0107351844138503,	0.000686843168696682,	-0.00687439142765975,	-0.00889586604750950,	-0.00800629000198628,	7.84883340792437e-05,	0.00127624416230591,	0.00281448125397012,	0.00253303203980908]])
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, color, width, height, x, y):
@@ -77,10 +83,10 @@ class Block(pygame.sprite.Sprite):
 
 block_group = pygame.sprite.Group()
 # initial position of players
-x = 700
-y = 50
-x1 = 700
-y1 = 600
+x  =  np.array([700])
+y  = np.array([50])
+x1 = np.array([710])
+y1 = np.array([600])
 actualX = 0
 actualY = 0
 # initial velocity of players
@@ -88,20 +94,16 @@ v_x = 0
 v_y = 0
 v_x1 = 0
 v_y1 = 0
-player = pygame.rect.Rect(x, y, 25, 25)
-player1 = pygame.rect.Rect(x1, y1, 25, 25)
-
-def calc_newPos(v_x, v_y, x, y):
-
-    return (x + v_x, y + v_y)
+attacker = pygame.rect.Rect(x.item(), y.item(), 25, 25)
+defender = pygame.rect.Rect(x1.item(), y1.item(), 25, 25)
 
 #record data of inputs every game in a list
 #append data at every 10 ms to a list - joystick and velocity info and positions of the players
 #posotion of flag and time it took to get it
 #save data to a file
 
-#change player1 to circle 
-#add a circle radius around player and flag to show capture radius 
+#change defender to circle 
+#add a circle radius around attacker and flag to show capture radius 
 #time score should be 0 for losing player
 
 # person whos capturing is the attacker/player
@@ -109,14 +111,23 @@ def calc_newPos(v_x, v_y, x, y):
 
 # 4 scoring conditions 
 
-# 1. player brought back flag (player full points, defender 0)
-# 2. defender intercepts (defender full points, player 0)
-# 3. time runs out (defender 75% of points, player 0) 
-# 4. player captures flag but dont bring it back (player 25% of points, defender 0)
+# 1. attacker brought back flag (attacker full points, defender 0)
+# 2. defender intercepts (defender full points, attacker 0)
+# 3. time runs out (defender 75% of points, attacker 0) 
+# 4. attacker captures flag but dont bring it back (attacker 25% of points, defender 0)
 
 #timing score 
-# player score: 20 - time it took for player to capture flag 
+# attacker score: 20 - time it took for attacker to capture flag 
 # defender: the time elapsed (or trial time)
+
+
+
+#============================ Function Space ==================================
+# Move to a separate file with class and its methods ==========================
+
+def vel_to_pos(v_x, v_y, x, y):
+    return np.array([x + v_x]), np.array([y + v_y])
+
 def draw_grid():
     blockSize = 133  # Set the size of the grid block
    # for x in range(0, 665, blockSize):
@@ -142,6 +153,7 @@ def fill_grid():
     blockY = y
     new_block = Block((255, 0, 0), blockSize, blockSize, x, y)
     block_group.add(new_block)
+    
     
 
 def show_score1(x, y):
@@ -205,11 +217,11 @@ def checkSafe(topleft):
     else:
         return False 
     
-def readValues():
-    if os.path.getsize(path) == 0:
+def readGloveData():
+    if os.path.getsize(filePath_gloveData) == 0:
         print("Error")
     else: 
-        with open (path, "r") as file: 
+        with open (filePath_gloveData, "r") as file: 
             reader = csv.reader(file)
             for row in reader:
                 return row
@@ -226,11 +238,12 @@ def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
-def combine_inputs(arr):
-    return m_arr @ arr
+def mapGlove_cursor(joint_angles, C_glove, p0):
+    return C_glove @ joint_angles - p0
 
-def combine_inputs1(arr):
-    return m_arr1 @ arr
+
+def mapJoy_cursor(joyInput):
+    return C_joy @ joyInput
 
 def checkDrift(axis):
     if abs(axis) < 0.1:
@@ -239,34 +252,42 @@ def checkDrift(axis):
         return axis
 
     
-def isSafe_player(x, y):
-    if x > 700 and y < 108 and captured == True:
+def isSafe_attacker(x, y):
+    if x > 666 and y < 108:
         return True
     else:
         return False
     
-def isSafe_player1(x, y):
-    if x>700 and y>532:
+def isSafe_defender(x, y):
+    if x>666 and y>532:
         return True
     else:
         return False
     
-def checkBounds(x, y, player):
-    if player:
+
+def chkBoundsArr(X, attacker):
+    if attacker:
+        if X[0] < 0:
+            X[0] = 0
+
+def checkBounds(x_arr, y_arr, attacker):
+    x = x_arr.item()
+    y = y_arr.item()
+    if attacker:
         if x < 0:
-            x = 0
-        if x > 774 and y <= 108:
+            x = 13
+        if x > 774 and y <= 121:
             x = 774
-        if x > 687 and y > 108:
-            y = 108
+        if x > 680 and y > 121:
+            y = 121
         if x > 774:
             x = 774
         if y < 0:
-            y = 0
-        if y > 115 and x > 640:
-            x = 640
+            y = 13
+        if y >= 141 and x > 654:
+            x = 654
         if y > 640:
-            y = 640
+            y = 656
     else: 
         if x < 0:
             x = 0
@@ -281,169 +302,214 @@ def checkBounds(x, y, player):
         if y > 640:
             y = 640
         
-    return (x, y)
+    return np.array([x]), np.array([y])
 
 
 while running:
-    arr = readValues()
-    arr = np.array(arr)
-    arr = arr.astype(np.float64)
-
- 
-    if (arr.shape == (21,)):
-        newarr = arr.reshape(21, 1)
-        
-   
-    if time.time() - timer >= 0.01 and block_group.sprites():
-        game_data.append( {
-            #'joystick': [joysticks[0].get_axis(0), joysticks[0].get_axis(1), joysticks[0].get_axis(2), joysticks[0].get_axis(3)],
-            #'velocity': [v_x, v_y, v_x1, v_y1],
-            'position': [(x, y), (x1, y1)],
-            'flag': block_group.sprites()[0].rect.center,
-            'trial': trial,
-        })
     
+    # Read Glove data for attacker
+    gloveInputRaw = readGloveData()
+    if not gloveInputRaw == None:
+        gloveInputArr = np.array(gloveInputRaw).astype(np.float64)[:, None]
+        
+    # print(gloveInputArr.shape)
+    
+    # Take joystick inputs and map to defender cursor's velocity -> position
+    if plugged:
+        joyInputArr = np.array([checkDrift(joysticks[0].get_axis(0)), checkDrift(joysticks[0].get_axis(1)), checkDrift(joysticks[0].get_axis(2)), checkDrift(joysticks[0].get_axis(3))])
+        v_x1, v_y1 = mapJoy_cursor(joyInputArr)
+        x1, y1 = vel_to_pos(v_x1, v_y1, x1, y1)
+        
+   # # saving data?
+   #  if time.time() - timer >= 0.01 and block_group.sprites():
+   #      game_data.append( {
+   #          #'joystick': [joysticks[0].get_axis(0), joysticks[0].get_axis(1), joysticks[0].get_axis(2), joysticks[0].get_axis(3)],
+   #          #'velocity': [v_x, v_y, v_x1, v_y1],
+   #          'position': [(x, y), (x1, y1)],
+   #          'flag': block_group.sprites()[0].rect.center,
+   #          'trial': trial,
+   #      })
+    
+   # make sure it only runs once per trial 
     start_time = time.time()
 
+    # change name of variable
     if block_group.sprites():
-        s = block_group.sprites()[0]
-    
-    #control mouse with joystick
-    ##x, y = calc_newPos(v_x, v_y, x, y)
-    x1, y1 = calc_newPos(v_x1, v_y1, x1, y1)
-
-
-    #print(joysticks[0].get_axis(0), joysticks[0].get_axis(1))
-    #stick drift 
-
-    # Restrict players within the playing area
-
-
-    # Update player positions
-    
-    player.topleft = (x, y)
-    player1.center = (x1, y1)
-    if attackerCaptured: 
-        s.kill()
-        screen.fill((255, 255, 255))
-        draw_grid()
-        show_warning()
-        if isSafe_player(x, y) == True and isSafe_player1(x1, y1) == True:
-            trial += 1
-            player_score += (20-int(time.time() - timer))
-            captured = False
-            attackerCaptured = False
-            timer = time.time()
-            color = (0, 0, 255)
-            a = 0
-            block_group.draw(screen)
-    elif not captured and not attackerCaptured:
-            block_group.draw(screen)
-            pygame.draw.circle(screen, (0, 255, 0), (blockX, blockY), 150, 5)
-            pygame.draw.circle(screen, (0, 0, 255), (blockX, blockY), 80, 3)
-            draw_grid()
-    
-    pygame.draw.rect(screen, color, player)
-    pygame.draw.rect(screen, (0, 255, 0), player1)
-    
-    # make a 4 by 1 matrix of the joystick inputs
-    if plugged:
-        arr1 = np.array([checkDrift(joysticks[0].get_axis(0)), checkDrift(joysticks[0].get_axis(1)), checkDrift(joysticks[0].get_axis(2)), checkDrift(joysticks[0].get_axis(3))])
-
-    if plugged:
-        v_x1, v_y1 = combine_inputs1(arr1)
-    # make x and y the outputs of the matrix multiplication
-    
-    if (newarr.shape == (21, 1)):
-        x, y = combine_inputs(newarr)
-        actualX, actualY = combine_inputs(newarr)
-        x = x*200
-        y = y*200
+        flagBlock = block_group.sprites()[0] 
         
-        #v_x1, v_y1 = combine_inputs(arr1)
-    
-    x, y = checkBounds(x, y, True)
-    x1, y1 = checkBounds(x1, y1, False)
+    # drawing attacker and defender on screen
+    pygame.draw.rect(screen, attackerColor, attacker)
+    pygame.draw.rect(screen, (0, 255, 0), defender)
+        
+    # make x and y the outputs of the matrix multiplication    
+    x, y = mapGlove_cursor(gloveInputArr, C_glove, p0)
+    actualX, actualY = x, y
+    print(x, y)
+    # Ensuring player positions are in bounds
+    x, y = checkBounds(x, y, attacker=True)
+    x1, y1 = checkBounds(x1, y1, attacker=False)
+    print(x, y)
+    # print(x, y, x1, y1)
+    # abc
+    attacker.center = (x.item(), y.item())
+    defender.center = (x1.item(), y1.item())
 
 
+    # checking for game stop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == JOYDEVICEADDED:
             print("Joystick connected")
+            
 
-    # problem, block spawns in the same place as the last one
-    if (s.checkMouse(player.topleft) and not attackerCaptured):
-        if not captured:
-            captured = True
-            color = (255, 0, 0)
-            s.kill()
-            screen.fill((255, 255, 255))
-            a = 0
-            draw_grid()
-            fill_grid()
-
+            
+    # checking if defender has crossed their boundary
     if (np.linalg.norm(np.array([x1, y1]) - np.array([blockX, blockY])) < 150):
         print(x1, y1)
         _theta = atan2((y1-blockY),(x1-blockX))
         print(_theta)
-        x1 = blockX + 151*math.cos(_theta)
+        x1 = np.array([blockX + 151*math.cos(_theta)])
         print(x1)
-        y1 = blockY + 151*math.sin(_theta)
+        y1 = np.array([blockY + 151*math.sin(_theta)])
         print(y1)
-
+    
     if (np.linalg.norm(np.array([x1, y1]) - np.array([666, 0])) < 200): 
         _theta = atan2((y1-0),(x1-666))
-        x1 = 666 + 201*math.cos(_theta)
-        y1 = 0 + 201*math.sin(_theta)
+        x1 =np.array([666 + 201*math.cos(_theta)])
+        y1 = np.array([0 + 201*math.sin(_theta)])
     
-    
-    
-    if (np.linalg.norm(np.array([x1, y1]) - np.array([int(x), int(y)])) < 25):
-        captured = False
-        color = (0, 0, 255)
-        show_warning()
-        attackerCaptured = True
-        x1 = 700
-        y1 = 600
+    # check for when attacker captures flag
+    if (flagBlock.checkMouse(attacker.center) and not attackerCaptured):
+        flagCaptured = True
+        
+        # if not captured:
+        #     captured = True
+        #     color = (255, 0, 0)
+        #     s.kill()
+        #     screen.fill((255, 255, 255))
+        #     a = 0
+        #     draw_grid()
+        #     fill_grid()
+            
+    # checking if defender captures attacker
+    if (np.linalg.norm(np.array([x1, y1]) - np.array([x, y])) < 25):
+        attackerCaptured = True 
+        flagCaptured = False
+        
     
         
-   
-#    if (np.linalg.norm(np.array([x1, y1])-np.array([x, y])) < 30):
- #       captured = False
-  #      color = (0, 0, 255)
-        
-  #      show_warning()
-    
-    # second controller
-    # safe zone
-    if isSafe_player(x, y) == True and isSafe_player1(x1, y1) == True:
-        trial += 1
-        player_score += (20-int(time.time() - timer))
-        x1 = 700
-        y1 = 600
-        game_data.append({
-            'time_to_capture': int(time.time() - timer),
-        })
-        captured = False
-        timer = time.time()
-        color = (0, 0, 255)
-        a = 0
+    # If attacker gets captured by defender 
+    # if attackerCaptured: 
+    #     print("test!")
+    #     s.kill()
+    #     screen.fill((255, 255, 255))
+    #     draw_grid()
+    #     show_warning()
+    #     if isSafe_player(x, y) == True and isSafe_player1(x1, y1) == True:
+    #         trial += 1
+    #         player_score += (20-int(time.time() - timer))
+    #         captured = False
+    #         attackerCaptured = False
+    #         timer = time.time()
+    #         color = (0, 0, 255)
+    #         a = 0
+    #         block_group.draw(screen)
+    #         print("wtf")
+            
+    # If attacker is not captured and flag is also not captured
+    if not hasFlag:   
         block_group.draw(screen)
+        pygame.draw.circle(screen, (0, 255, 0), (blockX, blockY), 150, 5)
+        pygame.draw.circle(screen, (0, 0, 255), (blockX, blockY), 80, 3)
+        draw_grid()
+    
+
+    
+    # checks if both players are in safe zone, then resets trial
+    # if isSafe_player(x, y) == True and isSafe_player1(x1, y1) == True:
+    #     captured = False
+    #     print("Test!")
+    #     trial += 1
+    #     player_score += (20-int(time.time() - timer))
+    #     x1 = np.array([710])
+    #     y1 = np.array([600])
+    #     game_data.append({
+    #         'time_to_capture': int(time.time() - timer),
+    #     })
+        
+    #     timer = time.time()
+    #     color = (0, 0, 255)
+    #     a = 0
+        
 
     # time doesnt start counting until both players leave the safe zone
     if (x > 665 and y < 134 and x1 > 665 and y1 > 532):
        timer = time.time()
-
-    """
-    if (isSafe_player(x, y) == True):
-        captured = False 
-        color = (0, 0, 255) 
+    
+    # checking conditions 
+    if flagCaptured: 
+        attackerColor = (255, 0, 0)
+        flagBlock.kill()
+        screen.fill((255, 255, 255))
+        draw_grid()
+        fill_grid()
+        hasFlag = True
+        flagCaptured = False
+    
+    if hasFlag: 
+        draw_grid()
+        attackerColor = (255, 0, 0)
+        # checks if attacker returns to safe zone with flag 
+        if isSafe_attacker(x.item(), y.item()) == True:
+            print("TEST!!!!")
+            attackerColor = (0, 0, 255)
+            hasFlag = False
+            returnHome_flag = True
+        
+    if attackerCaptured: 
+        attackerColor = (0, 0, 255)
+        flagBlock.kill()
+        screen.fill((255, 255, 255))
+        draw_grid()
+        returnHome_flag = True
+        
+    if flagReturned:
+        attackerColor = (0, 0, 255)
+        returnHome_flag = True  
+        
+    if returnHome_flag: 
+        print("Return home")
+        screen.fill((255, 255, 255))
+        draw_grid()
+        show_warning()
+        # checks if attacker and defender are both in safe zones 
+        if isSafe_attacker(x.item(), y.item()) == True and isSafe_defender(x1.item(), y1.item()) == True:
+            trialEnd_flag = True
+            returnHome_flag = False
+        
+    if trialEnd_flag: 
+        print("TEST")
+        #save data 
+        if trial < 59:
+            print("test")
+            trial += 1 
+        else: 
+            trial = 1 
+            session += 1 
+        
+        #scoring stuff
+        player_score += (20-int(time.time() - timer))
+        
+        #resets all flags
+        flagCaptured = False
+        attackerCaptured = False
+        flagReturned = False 
+        returnHome_flag = False
+        trialEnd_flag = False
+        
         timer = time.time()
-        trial += 1
-
-        #timer = time.time()
-    """
+        
     clock.tick(60)
     point = pygame.mouse.get_pos()
     show_score1(textX, textY)
@@ -453,6 +519,10 @@ while running:
     pygame.display.flip()
     
     screen.fill((255, 255, 255))
+    
+    # Trial end jobs: Save data, incrememnt trial number, select next flag position, compute the score to be displayed on next trial, reset other flags
+    # if trialEnd_flag:
+        
 
 with open ('data.pkl', 'wb') as f:
     pickle.dump(game_data, f)
@@ -466,4 +536,5 @@ pygame.quit()
 # Bounds for Flag and Home position of player 
 # Actual x,y and displayed x,y 
 # No time limit for trial, message to tell both players to go back to home position before new trial starts 
+
 # Circle around flag and attacker 
